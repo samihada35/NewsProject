210d54406efae6d5f0bc9f4fd0efac2d41aff849
Samihada35 <vitasant35@gmail.com>
2021-07-20 22:25:34 +0300

    Add pages, styles, fonts
    Add main & contact page, fonts & async load of fonts

--- styles/style.js
+++ styles/style.js
@@ -4,10 +4,12 @@ export const gStyle = StyleSheet.create({
   main: {
     flex: 1,
     padding: 20,
+    paddingTop: 60,
   },
   title: {
     fontSize: 20,
     color: "#333",
-    //fontFamily: ''
+    fontFamily: "mt-bold",
+    textAlign: "center",
   },
 });
