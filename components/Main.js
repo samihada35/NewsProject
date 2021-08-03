52828920c05a9e54c3a458b7371e146a9fe22718
Samihada35 <vitasant35@gmail.com>
2021-08-03 22:06:18 +0300

    add img
    add img & some minor style fix

--- components/Main.js
+++ components/Main.js
@@ -5,6 +5,7 @@ import {
   Text,
   TouchableOpacity,
   FlatList,
+  Image,
 } from "react-native";
 import { gStyle } from "../styles/style";
 
@@ -14,18 +15,32 @@ export default function Main({ navigation }) {
   // };
 
   const [news, setNews] = useState([
-    { name: "Google", anons: "Gooogle!!!!", full: "Google is cool" },
-    { name: "Apple", anons: "Apple!!!!", full: "Apple is very-very cool" },
+    {
+      name: "Google?",
+      anons: "Gooogle!!!!",
+      full: "Google is cool",
+      key: "1",
+      img: "https://clck.ru/WbtX2",
+    },
+    {
+      name: "Apple",
+      anons: "Apple!!!!",
+      full: "Apple is very-very cool",
+      key: "2",
+      img: "https://clck.ru/WbtX2",
+    },
     {
       name: "Microsoft",
       anons: "Microsoft!!!!",
       full: "Microsoft is not cool",
+      key: "3",
+      img: "https://clck.ru/WbtX2",
     },
   ]);
 
   return (
     <View style={gStyle.main}>
-      <Text style={gStyle.title}>Главная страница</Text>
+      <Text style={gStyle.sTitle}>Главная страница</Text>
       <FlatList
         data={news}
         renderItem={({ item }) => (
@@ -34,8 +49,15 @@ export default function Main({ navigation }) {
               navigation.navigate("FullInfo", item);
             }}
           >
-            <Text>{item.name}</Text>
-            <Text>{item.anons}</Text>
+            <Text style={gStyle.title}>{item.name}</Text>
+            <Image
+              source={{
+                width: "100%",
+                height: 200,
+                uri: item.img,
+              }}
+            />
+            <Text style={gStyle.text}>{item.anons}</Text>
           </TouchableOpacity>
         )}
       />
--- styles/style.js
+++ styles/style.js
@@ -4,12 +4,25 @@ export const gStyle = StyleSheet.create({
   main: {
     flex: 1,
     padding: 20,
-    paddingTop: 60,
+    paddingTop: 30,
   },
   title: {
-    fontSize: 20,
+    fontSize: 18,
     color: "#333",
     fontFamily: "mt-bold",
     textAlign: "center",
+    marginBottom: 20,
+  },
+  sTitle: {
+    fontSize: 20,
+    color: "#333",
+    fontFamily: "mt-light",
+    textAlign: "center",
+    marginBottom: 20,
+  },
+  text: {
+    fontSize: 15,
+    marginBottom: 20,
+    color: "#333",
   },
 });
